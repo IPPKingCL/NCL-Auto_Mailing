79b599bf2985eb412840b3c6a52e2d83a38daf32
Hwang-YoonGyu <dotheji11@naver.com>
2022-08-11 09:44:55 +0900

    2022-08-11 09:44

--- src/main/java/kir/nclcorp/comm/HomeController.java
+++ src/main/java/kir/nclcorp/comm/HomeController.java
@@ -3,13 +3,8 @@ package kir.nclcorp.comm;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private customThread ct = customThread.instance;
+	@Autowired
+	MailService emailService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -30,24 +28,12 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+
+		System.out.println("안녕@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+		emailService.sendSimpleMessage("qudqud97@nclworks.com","Test","테스트 이메일입니다.");
+
+
 		return "home";
-	}
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.mailplug.co.kr");
-		mailSender.setPort(587);
-
-		mailSender.setUsername("yoongyu@nclworks.com");
-		mailSender.setPassword("net123789!");
-
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-
-		return mailSender;
 	}
 }
