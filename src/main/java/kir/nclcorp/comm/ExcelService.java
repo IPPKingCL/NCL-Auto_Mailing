509aa3b812f279e7cf974830395c34a3c27cabec
hjkwon0814 <hjkwon0814@naver.com>
2022-08-12 16:40:22 +0900

    add remark on excel service class

--- src/main/java/kir/nclcorp/comm/ExcelService.java
+++ src/main/java/kir/nclcorp/comm/ExcelService.java
@@ -36,7 +36,7 @@ public class ExcelService {
             FileInputStream inputStream = new FileInputStream(filePath);
             XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputStream);
             sheet = xssfWorkbook.getSheet("rawdata");
-            sheet.autoSizeColumn(1);
+            sheet.autoSizeColumn(1); // 셀 사이즈 자동 조절
 
             rowIndex = findBlankRowIndex(sheet);
             XSSFRow row;
@@ -48,19 +48,18 @@ public class ExcelService {
                 cell1 = row.createCell(1);
                 cell2 = row.createCell(2);
                 cell3 = row.createCell(3);
-                cell4 = row.createCell(4);
+                cell4 = row.createCell(4); // 셀 생성
 
-                System.out.println("date = " + date + " rowIndex = " + rowIndex + " seq = " + seq + " key = " + key + " ApiData.get(key) = " + ApiData.get(key));
                 cell1.setCellValue(date);
                 cell2.setCellValue(Integer.parseInt(key));
                 cell3.setCellValue(ApiData.get(key));
-                cell4.setCellValue(seq);
+                cell4.setCellValue(seq); // 생선된 셀에 값 입력
                 rowIndex++;
             }
             XSSFRow finalIndexRow = sheet.createRow(finalIndexrow);
             finalcell = finalIndexRow.createCell(finalIndexcol);
             finalcell.setCellValue(rowIndex);
-            xssfWorkbook.write(fos);
+            xssfWorkbook.write(fos); // 입력
             fos.close();
 
         } catch (IOException e) {
@@ -68,16 +67,16 @@ public class ExcelService {
         }
     }
 
-    public Integer findBlankRowIndex(XSSFSheet sheet) {
+    public Integer findBlankRowIndex(XSSFSheet sheet) { //엑셀 안에 있는 마지막 인덱스 번호를 불러와서 저장된 데이터 이후부터 다시 작성하도록 하는 함수
         int finalRow;
-        int rowIndex = 0;
-        int colIndex = 2;
+        int rowIndex = 0; //마지막으로 작성한 셀의 행 인덱스
+        int colIndex = 2; // 마지막으로 작성한 셀의 열 인덱스
         Double tempfinalIndex;
         XSSFRow row = sheet.getRow(rowIndex);
         XSSFCell cell = row.getCell(colIndex);
 
-        tempfinalIndex = cell.getNumericCellValue();
-        finalRow = tempfinalIndex.intValue();
+        tempfinalIndex = cell.getNumericCellValue(); // 더블형 반환
+        finalRow = tempfinalIndex.intValue(); // 정수로 변환
 
         return finalRow;
     }
